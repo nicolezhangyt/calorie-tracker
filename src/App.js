f89c0199d2d122e3dde85c2b5b51979b45cb56ac
Yating <nicole@churinc.com>
2020-01-16 00:53:39 +1100

    initialized project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import InfoPage from "./components/InfoPage";
+
+function App() {
+  return <InfoPage />;
+}
+
+export default App;
---
+++ src/components/InfoPage.js
@@ -0,0 +1,89 @@
+import React, { Component } from "react";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
+import NavigateNextIcon from "@material-ui/icons/NavigateNext";
+
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    background: "#6200ED"
+  },
+  toolbar: {
+    minHeight: 80,
+    alignItems: "center",
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(2)
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: "white",
+    color: "black",
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto"
+    }
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  inputRoot: {
+    color: "inherit"
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: 200
+    }
+  },
+  time: {
+    fontSize: "22px",
+    marginLeft: "30px",
+    marginRight: "30px"
+  },
+  timeSelection: {
+    flexGrow: 1,
+    alignSelf: "flex-end"
+  }
+}));
+export default function InfoPage() {
+  const classes = useStyles();
+  return (
+    <AppBar className={classes.appBar} position="static">
+      <Toolbar className={classes.toolbar}>
+        <div className={classes.search}>
+          <div className={classes.searchIcon}>
+            <SearchIcon />
+          </div>
+          <InputBase
+            placeholder="Search…"
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput
+            }}
+            inputProps={{ "aria-label": "search" }}
+          />
+        </div>
+        <div className={classes.timeSelection} noWrap>
+          <NavigateBeforeIcon fontSize="small" />
+          <span className={classes.time}>{"Today"}</span>
+          <NavigateNextIcon fontSize="small" />
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+}
